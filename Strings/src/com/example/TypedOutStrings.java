716942b113de3b2477065686738a9bdf39f896aa
kaushikpaul <kaushik.paul755@gmail.com>
2022-10-02 14:12:39 +0530

    Add Optimal TypedOutStrings

--- Strings/src/com/example/TypedOutStrings.java
+++ Strings/src/com/example/TypedOutStrings.java
@@ -22,10 +22,49 @@ public class TypedOutStrings {
         return buildString(string1).equals(buildString(string2));
     }
 
+    // O(n) time and O(1) space
+    public static boolean optimalBackspaceString(String string1, String string2) {
+        int pointer1 = string1.length() - 1, pointer2 = string2.length() - 1;
+        while (pointer1 >= 0 || pointer2 >= 0) {
+            if (string1.charAt(pointer1) == '#' || string2.charAt(pointer2) == '#') {
+                int backspace = 2;
+                if (string1.charAt(pointer1) == '#') {
+                    while (backspace > 0) {
+                        if (pointer1 > 0) {
+                            pointer1--;
+                        }
+                        backspace--;
+                        if (string1.charAt(pointer1) == '#') {
+                            backspace += 2;
+                        }
+                    }
+                } else {
+                    while (backspace > 0) {
+                        if (pointer2 > 0) {
+                            pointer2--;
+                        }
+                        backspace--;
+                        if (string2.charAt(pointer2) == '#') {
+                            backspace += 2;
+                        }
+                    }
+                }
+            } else {
+                if (string1.charAt(pointer1) != string2.charAt(pointer2)) {
+                    return false;
+                } else {
+                    pointer1--;
+                    pointer2--;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String string1 = "abc#d";
         String string2 = "abzz##d";
 
-        System.out.println("Is Both the string equal: " + compareStrings(string1, string2));
+        System.out.println("Is Both the string equal: " + optimalBackspaceString(string1, string2));
     }
 }
