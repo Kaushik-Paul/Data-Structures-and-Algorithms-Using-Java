06f76b6de6386f966738a4cc82e47674e4021f13
kaushikpaul <kaushik.paul755@gmail.com>
2022-10-22 12:55:27 +0530

    Update MaximumSubarray

--- Greedy Algorithms/src/com/example/MaximumSubarray.java
+++ Greedy Algorithms/src/com/example/MaximumSubarray.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class MaximumSubarray {
 
+    // O(n) time and O(1) space
     public static int maximumSubarray(int[] array) {
         int maxSub = array[0];
 
@@ -16,8 +17,20 @@ public class MaximumSubarray {
         return maxSub;
     }
 
+    // General Approach
+    // O(n) time and O(1) space
+    public static int maximumSubarrayV2(int[] array) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for (int num : array) {
+            currentSum = Math.max(currentSum + num, num);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args) {
         int[] array = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println("The Maximum Subarray is: " + maximumSubarray(array));
+        System.out.println("The Maximum Subarray is: " + maximumSubarrayV2(array));
     }
 }
