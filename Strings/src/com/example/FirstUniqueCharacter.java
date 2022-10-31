b8b6ac8ef52924a9759025777a64a08a5d900e12
kaushikpaul <kaushik.paul755@gmail.com>
2022-10-31 15:16:22 +0530

    Add FirstUniqueCharacter

---
+++ Strings/src/com/example/FirstUniqueCharacter.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class FirstUniqueCharacter {
+
+    public static int firstUniqueCharacter(String string) {
+        int[] frequency = new int[26];
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            frequency[ch - 'a'] += 1;
+        }
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (frequency[ch - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String string = "loveleetcode";
+        System.out.println("The First Unique Character is at position: " + firstUniqueCharacter(string));
+    }
+}
