4482c316ea0e95684e4f76c9fd74e70e7c1db6f5
kaushikpaul <kaushik.paul755@gmail.com>
2022-12-23 12:38:03 +0530

    Add DecodeWaysV2

--- Dynamic Programming/src/com/example/DecodeWays.java
+++ Dynamic Programming/src/com/example/DecodeWays.java
@@ -20,8 +20,28 @@ public class DecodeWays {
         return decodeWays[string.length()];
     }
 
+    // O(n) time and O(n) space
+    public static int decodeWaysV2(String string) {
+        int[] decodeWays = new int[string.length() + 1];
+        // Base Case
+        decodeWays[0] = 1;
+        decodeWays[1] = string.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= string.length(); i++) {
+            int oneDigit = Integer.parseInt(string.substring(i - 1, i));
+            int twoDigit = Integer.parseInt(string.substring(i - 2, i));
+            if (oneDigit >= 1) {
+                decodeWays[i] += decodeWays[i - 1];
+            }
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                decodeWays[i] += decodeWays[i - 2];
+            }
+        }
+        return decodeWays[string.length()];
+    }
+
     public static void main(String[] args) {
         String string = "226";
-        System.out.println("The NUmber of ways to decode the sting is: " + decodeWays(string));
+        System.out.println("The Number of ways to decode the sting is: " + decodeWaysV2(string));
     }
 }
